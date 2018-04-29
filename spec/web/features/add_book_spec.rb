3e6ca9b9d362d13d4d3a10ff798a4eab60d9c6c0
Chris Mucheke <kchrismucheke@gmail.com>
2018-04-30 00:02:11 +0300

    Secure form validation and display validation errors

--- spec/web/features/add_book_spec.rb
+++ spec/web/features/add_book_spec.rb
@@ -1,21 +1,19 @@
 require 'features_helper'
 
-describe 'Add a book' do 
-    after do
-        BookRepository.new.clear
+describe 'Add a book' do
+  # Spec written earlier omitted for brevity
+
+  it 'displays list of errors when params contains errors' do
+    visit '/books/new'
+
+    within 'form#book-form' do
+      click_button 'Create'
     end
 
-    it 'can create a new book' do
-        visit '/books/new'
+    current_path.must_equal('/books')
 
-        within 'form#book-form' do
-            fill_in 'Title', with: 'New Book'
-            fill_in 'Author', with: 'Some Author'
-
-            click_button 'Create'
-        end 
-
-        current_path.must_equal('/books')
-        assert page.has_content?('New Book')
-    end 
-end 
+    assert page.has_content?('There was a problem with your submission')
+    assert page.has_content?('Title must be filled')
+    assert page.has_content?('Author must be filled')
+  end
+end
